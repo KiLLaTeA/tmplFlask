75d73e91cf4fff70e0aac25b5f8d288eb312372e
Maxim Kostin <poster173524@mail.ru>
2024-05-21 11:47:50 +0300

    Создал API и документацию.

--- flsite.py
+++ flsite.py
@@ -1,9 +1,12 @@
+import os
+import json
+import uuid
 import pickle
 import numpy as np
 import pandas as pd
 import sklearn
 from sklearn import preprocessing
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, jsonify, request
 
 scaler = preprocessing.MinMaxScaler()
 
@@ -22,9 +25,44 @@ loaded_model_tree = pickle.load(open('model/titanic_tree', 'rb'))
 results = ['не выжил', 'выжил']
 
 
+def read_data():
+    if not os.path.exists('logs.json'):
+        return []
+    with open('logs.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def write_data(logs):
+    with open('logs.json', 'w') as f:
+        json.dump(logs, f, indent=4)
+
+
 @app.route("/")
 def index():
     return render_template('index.html', title="Главная", menu=menu)
+
+
+@app.route('/api/logs', methods=['GET'])
+def get_records():
+    data = read_data()
+    return jsonify(data)
+
+
+@app.route('/api/logs/<float:age_current>', methods=['GET'])
+def get_record(age_current):
+    data = read_data()
+    record = next((item for item in data if item['Age'] == float(age_current)), None)
+    if record:
+        return jsonify(record)
+    else:
+        return jsonify({'error': 'Записи с таким возрастом не найдены!'}), 404
+
+
+@app.route("/docs", methods=['GET'])
+def docs():
+    if request.method == 'GET':
+        return render_template('docs.html', title="Документация", menu=menu, class_model='')
 
 
 @app.route("/p_knn", methods=['POST', 'GET'])
@@ -40,6 +78,14 @@ def f_lab1():
                                float(request.form['Parents/Children Aboard']),
                                float(request.form['Fare'])]])
             pred = results[loaded_model_knn.predict(X_new)[0]]
+
+            data = read_data()
+            form_data = request.form.to_dict()
+            id_record = len(data) + 1
+            record = {'id': id_record, **form_data}
+            data.append(record)
+            write_data(data)
+
             return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu, class_model="Этот человек " + str(pred))
         except:
             return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu, request='GET')
@@ -58,8 +104,23 @@ def f_lab2():
                                float(request.form['Parents/Children Aboard']),
                                float(request.form['Fare'])]])
             pred = results[loaded_model_logistic.predict(X_new)[0]]
+            data = read_data()
+            logs = {
+                "id": str(uuid.uuid4()),
+                "Pclass": float(request.form['Pclass']),
+                "Sex": float(request.form['Sex']),
+                "Age": float(request.form['Age']),
+                "Siblings/Spouses Aboard": float(request.form['Siblings/Spouses Aboard']),
+                "Parents/Children Aboard": float(request.form['Parents/Children Aboard']),
+                "Fare": float(request.form['Fare']),
+            }
+            data.append(logs)
+            write_data(data)
             return render_template('lab2.html', title="Логистическая регрессия", menu=menu, class_model="Этот человек " + str(pred))
-        except:
+        except Exception as e:
+            # Обработка ошибки и вывод её типа и сообщения
+            print(f"Произошла ошибка: {type(e).__name__}")
+            print(f"Сообщение об ошибке: {e}")
             return render_template('lab2.html', title="Логистическая регрессия", menu=menu, request='GET')
 
 
